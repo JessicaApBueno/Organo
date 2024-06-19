f08697409c3048cf6617809f4f181f087dfc751c
JessicaApBueno <buenojessicaaparecida@gmail.com>
2024-06-19 08:44:57 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+function Banner() {
+return(
+    <header className='Banner'>
+    <img src="./imagens/Banner.png" alt="Banner" />
+    </header>
+)
+
+}
+
+export default Banner
